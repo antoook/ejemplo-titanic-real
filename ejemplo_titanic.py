efba2aa250117c236bb9cd5f4d15456ae4fc7c23
antoook <antonoepastor1@gmail.com>
2025-11-17 16:02:11 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # Carga el archivo CSV "database_titanic.csv" en un DataFrame de pandas.
 df = pd.read_csv("database_titanic.csv")
@@ -15,20 +15,20 @@ st.write("""
 with st.sidebar:
     # Título para la sección de opciones en la barra lateral.
     st.write("# Opciones")
-    
+
     # Crea un control deslizante (slider) que permite al usuario seleccionar un número de bins
     # en el rango de 0 a 10, con un valor predeterminado de 2.
-    div = st.slider('Número de bins:', 1, 10, 2)
-    
+    div = st.slider('Número de bins:', 0, 10, 2)
+
     # Muestra el valor actual del slider en la barra lateral.
     st.write("Bins=", div)
 
 # Desplegamos un histograma con los datos del eje X
-#fig, ax = plt.subplots(1, 2, figsize=(10, 3))
-#ax[0].hist(df["Age"], bins=div)
-#ax[0].set_xlabel("Edad")
-#ax[0].set_ylabel("Frecuencia")
-#ax[0].set_title("Histograma de edades")
+fig, ax = plt.subplots(1, 2, figsize=(10, 3))
+ax[0].hist(df["Age"], bins=div)
+ax[0].set_xlabel("Edad")
+ax[0].set_ylabel("Frecuencia")
+ax[0].set_title("Histograma de edades")
 
 # Tomando datos para hombres y contando la cantidad
 df_male = df[df["Sex"] == "male"]
@@ -38,13 +38,13 @@ cant_male = len(df_male)
 df_female = df[df["Sex"] == "female"]
 cant_female = len(df_female)
 
-#ax[1].bar(["Masculino", "Femenino"], [cant_male, cant_female], color = "red")
-#ax[1].set_xlabel("Sexo")
-#ax[1].set_ylabel("Cantidad")
-#ax[1].set_title('Distribución de hombres y mujeres')
+ax[1].bar(["Masculino", "Femenino"], [cant_male, cant_female], color = "red")
+ax[1].set_xlabel("Sexo")
+ax[1].set_ylabel("Cantidad")
+ax[1].set_title('Distribución de hombres y mujeres')
 
 # Desplegamos el gráfico
-#st.pyplot(fig)
+st.pyplot(fig)
 
 st.write("""
 ## Muestra de datos cargados
