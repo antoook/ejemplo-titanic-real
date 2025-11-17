507a2eefc1ffa4de8085cac0c35673dcf8ee799b
antoook <antonoepastor1@gmail.com>
2025-11-17 15:59:46 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly
+import plotly.express as px
 # import matplotlib.pyplot as plt
 
 # Carga el archivo CSV "database_titanic.csv" en un DataFrame de pandas.
